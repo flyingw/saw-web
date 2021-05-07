3e6626486ceddb4529635686df0a1dc740dddf5e
tellnobody1 <6303577+tellnobody1@users.noreply.github.com>
2021-05-07 17:09:01 +0300

    update login widget

--- public/app.js
+++ public/app.js
@@ -10581,7 +10581,7 @@ var PS = {};
         window["telegramLoginF"] = f
         var element = document.createElement('script');
         element.setAttribute('async', '');
-        element.setAttribute('src', 'https://telegram.org/js/telegram-widget.js?9');
+        element.setAttribute('src', 'https://telegram.org/js/telegram-widget.js?14');
         element.setAttribute('data-telegram-login', 'RideHubCityBot');
         element.setAttribute('data-size', 'large');
         element.setAttribute('data-onauth', 'telegramLoginF(user)()');
